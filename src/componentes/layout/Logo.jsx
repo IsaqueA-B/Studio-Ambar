cafbf4897d30073b564bdd1a1900bf39c11bbfcb
quagmire77 <cassioschultz0703@gmail.com>
2026-06-16 19:55:32 -0300

    Testando

--- src/componentes/layout/Logo.jsx
+++ src/componentes/layout/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = () => (
   <div className="logo-container">
-    <span className="logo-texto">Nome do Projeto</span>
+    <span className="logo-texto">Nome do Projetaaaaa</span>
   </div>
 );
 
