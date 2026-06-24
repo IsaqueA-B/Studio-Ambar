b1f39839cc0e22b948114c920b28bf6f17c5f064
Isaque Bastos <isaquebastos@gmail.com>
2026-06-23 21:01:54 -0300

    Isaque-1°Seção_Home-23_06_2026

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,36 @@ function Home() {
                 </p>
             </section>
 
+            {/* SEÇÃO 1: QUEM SOMOS + SERVIÇOS EM DESTAQUE (dividida por linha) */}
+            <section className="home-secao1">
+                <div className="home-lado home-esquerda">
+                    <span className="sobre-tag">Quem Somos</span>
+                    <h2 className="home-titulo-esquerda">Criatividade que conecta</h2>
+                    <p>
+                        Transformamos ideias em marcas autênticas. Combinamos estratégia,
+                        estética e propósito para dar vida a negócios que se destacam no digital.
+                        Tudo para ficar do jeito que você imaginou, com a nossa assinatura de qualidade.
+                    </p>
+                    <Link to="/sobre" className="btn btn-outline">Saiba mais</Link>
+                </div>
+
+                {/* Linha divisória vertical */}
+                <div className="home-linha"></div>
+
+                <div className="home-lado home-direita">
+                    <span className="sobre-tag">Serviços em Destaque</span>
+                    <h2 className="home-titulo-direita">O que fazemos de diferente</h2>
+                        <p>
+                            Identidade Visual <br></br>
+                            Social Media <br></br>
+                            Branding Completo <br></br>
+                            Estilização personalizada
+                        </p>
+                    <Link to="/servicos" className="btn btn-outline">Ver todos os serviços</Link>
+                </div>
+            </section>
+
+
             <div className="grid-3x3 mb-20">
                 <div className="card">
                     <h3>Hero Banner</h3>
