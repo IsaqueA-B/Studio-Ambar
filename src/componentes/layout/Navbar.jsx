3730a8a84a0d9d9efea57ea4025abaef5e4948e5
Isaque Bastos <isaquebastos@gmail.com>
2026-06-30 20:41:29 -0300

    Isaque-Reatribuição_NavSuperior-30_06_2026

--- src/componentes/layout/Navbar.jsx
+++ src/componentes/layout/Navbar.jsx
@@ -1,17 +1,15 @@
 import { NavLink, Link, useNavigate } from 'react-router-dom';
 import { useUser } from '../users/UserContext';
-import logo1 from '../../assets/images/Especificos/Logo1.png';
+import logo from '../../assets/images/Especificos/LogoCompleta.png';
 import iconeSair from '../../assets/images/Icons/btn-sair.png';
 
-// -------------------- ÍCONES DA SIDEBAR --------------------
+// Icons da navegação
 import iconHome from '../../assets/images/Icons/icon-home.png';
 import iconSobre from '../../assets/images/Icons/icon-sobre.png';
 import iconServicos from '../../assets/images/Icons/icon-servicos.png';
 import iconPortfolio from '../../assets/images/Icons/icon-portfolio.png';
-import iconCases from '../../assets/images/Icons/icon-cases.png';
 import iconBlog from '../../assets/images/Icons/icon-blog.png';
 import iconContato from '../../assets/images/Icons/icon-contato.png';
-// -----------------------------------------------------------
 
 function Navbar() {
     const { user, logout } = useUser();
@@ -23,56 +21,56 @@ function Navbar() {
     };
 
     return (
-        <aside className="sidebar">
-            {/* Logo clicável -> Home */}
-            <Link to="/" className="sidebar-logo">
-                <img src={logo1} alt="Studio Âmbar" />
+        <header className="navbar">
+            {/* Logo à esquerda */}
+            <Link to="/" className="navbar-logo">
+                <img src={logo} alt="Studio Âmbar" />
             </Link>
 
-            {/* Navegação principal */}
-            <nav className="sidebar-nav">
-                <ul className="sidebar-links">
+            {/* Navegação principal centralizada */}
+            <nav className="navbar-nav">
+                <ul className="navbar-links">
                     <li>
                         <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
-                            <img src={iconHome} alt="" className="sidebar-icon" />
+                            <img src={iconHome} alt="" className="navbar-icon" />
                             <span>Home</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/sobre" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
-                            <img src={iconSobre} alt="" className="sidebar-icon" />
+                            <img src={iconSobre} alt="" className="navbar-icon" />
                             <span>Sobre</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/servicos" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
-                            <img src={iconServicos} alt="" className="sidebar-icon" />
+                            <img src={iconServicos} alt="" className="navbar-icon" />
                             <span>Serviços</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/portfolio" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
-                            <img src={iconPortfolio} alt="" className="sidebar-icon" />
+                            <img src={iconPortfolio} alt="" className="navbar-icon" />
                             <span>Portfólio</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/blog" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
-                            <img src={iconBlog} alt="" className="sidebar-icon" />
+                            <img src={iconBlog} alt="" className="navbar-icon" />
                             <span>Blog</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/contato" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
-                            <img src={iconContato} alt="" className="sidebar-icon" />
+                            <img src={iconContato} alt="" className="navbar-icon" />
                             <span>Contato</span>
                         </NavLink>
                     </li>
                 </ul>
             </nav>
 
-            {/* Área do usuário (fixa no rodapé da sidebar) */}
-            <div className="sidebar-user">
+            {/* Área do usuário à direita */}
+            <div className="navbar-user">
                 {user ? (
                     <>
                         <span className="user-nome">{user.nome}</span>
@@ -87,7 +85,7 @@ function Navbar() {
                     </Link>
                 )}
             </div>
-        </aside>
+        </header>
     );
 }
 
